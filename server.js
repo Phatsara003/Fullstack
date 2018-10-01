79bc245b5022d117c9124c0b58e3aa06928ad956
phatsara <phatsara_ae@hotmail.com>
2018-10-01 11:05:55 +0700

    push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var pgp = require('pg-promise')(/*options*/)
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true')
+var db = pgp('postgres://jcdfmzgcegkkxy:1cc0dec1ae4fa101a6fc4696b090b7b90f0f3847eb819bba2c969986f380b44e@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d51ai2ptaqnqhd')
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
