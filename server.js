395cb4b2169ce9abca9d06672630949041cf4062
phatsara <phatsara_ae@hotmail.com>
2018-10-20 16:44:32 +0700

    products

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/users', function (req, res) {
 
 
 //update product
-app.post('/product/update',function(req,res){
+app.post('/products/update',function(req,res){
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
