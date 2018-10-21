3a676f94f7453854de79be4f4b52ce789b0890c1
phatsara <phatsara_ae@hotmail.com>
2018-10-21 22:54:50 +0700

    test

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/products', function (req, res) {
     var id = request.param('id');
     var sql = 'select * from products';
     if (id) {
-        sql += ' where products =' + id + ' ORDER BY  product_id ASC';
+        sql += ' where products =' + id + ' ORDER BY product_id ASC';
     }
     db.any(sql + ' ORDER BY product_id ASC')
         .then(function (data) {
